4597b94fe74dc59431ae3cb6bbadaef480aef824
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-11-07 12:37:58 -0800

    refactored evalquestion and displaymessage function

--- js/app.js
+++ js/app.js
@@ -9,55 +9,44 @@ var user = prompt('Welcome to my page.  Please enter your name.');
 
 var dogQuestion = prompt('Okay, ' + user + '.  Do you think I like dogs? (Y/N)').toUpperCase();
 // console.log('Likes dogs: ', dogQuestion);
-if (dogQuestion === 'Y' || dogQuestion === 'YES') {
-  alert('That\'s correct!');
-  userCount++;
-} else {
-  alert('That\'s incorrect');
-}
+evalQuestion(dogQuestion);
 
 var pizzaQuestion = prompt('Okay, ' + user + '.  Do you think I like pizza? (Y/N)').toUpperCase();
 // console.log('Likes pizza: ', pizzaQuestion);
-if (pizzaQuestion === 'Y' || pizzaQuestion === 'YES') {
-  alert('That\'s correct!');
-  userCount++;
-} else {
-  alert('That\'s incorrect');
-}
+evalQuestion(pizzaQuestion);
 
 var soccerQuestion = prompt('Okay, ' + user + '.  Do you think I like soccer? (Y/N)').toUpperCase();
 // console.log('Likes soccer: ', soccerQuestion);
-if (soccerQuestion === 'Y' || soccerQuestion === 'YES') {
-  alert('That\'s correct!');
-  userCount++;
-} else {
-  alert('That\'s incorrect');
-}
+evalQuestion(soccerQuestion);
 
 var hikingQuestion = prompt('Okay, ' + user + '.  Do you think I like hiking? (Y/N)').toUpperCase();
 // console.log('Likes hiking: ', hikingQuestion);
-if (hikingQuestion === 'Y' || hikingQuestion === 'YES') {
-  alert('That\'s correct!');
-  userCount++;
-} else {
-  alert('That\'s incorrect');
-}
+evalQuestion(hikingQuestion);
 
 var cardGameQuestion = prompt('Okay, ' + user + '.  Do you think I like card games? (Y/N)').toUpperCase();
 // console.log('Likes card games: ', cardGameQuestion);
-if (cardGameQuestion === 'Y' || cardGameQuestion === 'YES') {
-  alert('That\'s correct!');
-  userCount++;
-} else {
-  alert('That\'s incorrect');
+evalQuestion(cardGameQuestion);
+
+// Display custom message 
+displayCustomMessage(userCount);
+
+// function alerts user with correct/incorrect based of their guess
+function evalQuestion(userGuess) {
+  if (userGuess === 'Y' || userGuess === 'YES') {
+    alert('That\'s correct!');
+    userCount++;
+  } else {
+    alert('That\'s incorrect');
+  }
 }
 
-
-// alert user with final message based off their answers
-if (userCount > 4) {
-  alert('Hey, ' + user + '.  You answered most questions correctly.  Maybe you know me already!');
-} else if (userCount === 3) {
-  alert('Hey, ' + user + '.  You answered some questions correctly.  Maybe I haven\'t talked enough to you!');
-} else if (userCount < 3) {
-  alert('Hey, ' + user + '.  You answered most questions wrong.  You don\'t know me well, but that\'s cool!');
+// function alerts user with final message based off the number of their correct answers
+function displayCustomMessage(count) {
+  if (count > 4) {
+    alert('Hey, ' + user + '.  You answered most questions correctly.  Maybe you know me already!');
+  } else if (count === 3) {
+    alert('Hey, ' + user + '.  You answered some questions correctly.  Maybe I haven\'t talked enough to you!');
+  } else if (count < 3) {
+    alert('Hey, ' + user + '.  You answered most questions wrong.  You don\'t know me well, but that\'s cool!');
+  }
 }

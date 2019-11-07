f4855c64a1cace0025996cc2d0bc0c9f4d5bf7bb
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-11-07 13:00:16 -0800

    refactored functions

--- js/app.js
+++ js/app.js
@@ -8,27 +8,32 @@ var user = prompt('Welcome to my page.  Please enter your name.');
 // console.log('User name:', user);
 
 var dogQuestion = prompt('Okay, ' + user + '.  Do you think I like dogs? (Y/N)').toUpperCase();
-// console.log('Likes dogs: ', dogQuestion);
 evalQuestion(dogQuestion);
+// console.log('Likes dogs: ', dogQuestion);
 
 var pizzaQuestion = prompt('Okay, ' + user + '.  Do you think I like pizza? (Y/N)').toUpperCase();
-// console.log('Likes pizza: ', pizzaQuestion);
 evalQuestion(pizzaQuestion);
+// console.log('Likes pizza: ', pizzaQuestion);
 
 var soccerQuestion = prompt('Okay, ' + user + '.  Do you think I like soccer? (Y/N)').toUpperCase();
-// console.log('Likes soccer: ', soccerQuestion);
 evalQuestion(soccerQuestion);
+// console.log('Likes soccer: ', soccerQuestion);
 
 var hikingQuestion = prompt('Okay, ' + user + '.  Do you think I like hiking? (Y/N)').toUpperCase();
-// console.log('Likes hiking: ', hikingQuestion);
 evalQuestion(hikingQuestion);
+// console.log('Likes hiking: ', hikingQuestion);
 
 var cardGameQuestion = prompt('Okay, ' + user + '.  Do you think I like card games? (Y/N)').toUpperCase();
-// console.log('Likes card games: ', cardGameQuestion);
 evalQuestion(cardGameQuestion);
+// console.log('Likes card games: ', cardGameQuestion);
 
 // Display custom message 
 displayCustomMessage(userCount);
+
+
+//
+// FUNCTION SECTION
+//
 
 // function alerts user with correct/incorrect based of their guess
 function evalQuestion(userGuess) {

664aa2eb1b66cc0d51464ac1d488c67cf3878771
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-11-07 14:23:51 -0800

    added functions for color game

--- js/app.js
+++ js/app.js
@@ -27,10 +27,13 @@ var cardGameQuestion = prompt('Okay, ' + user + '.  Do you think I like card gam
 evalQuestion(cardGameQuestion);
 // console.log('Likes card games: ', cardGameQuestion);
 
-// User attemps to guess the special number
+// User interacts with prompt in attempt to guess the special number
 questionGuessNum();
 
-// Display custom message 
+// User interacts with prompt in attempt to guess one of my favorite colors
+guessColor()
+
+// Display custom message based off how many answers user got right
 displayCustomMessage(correctUserGuessCount);
 
 
@@ -48,6 +51,7 @@ function evalQuestion(userGuess) {
   }
 }
 
+
 // function interacts with user based of their guess of the special number of 17
 function questionGuessNum() {
   // track num of guesses
@@ -59,6 +63,7 @@ function questionGuessNum() {
     // eval guess
     if (userNumberGuess === 17) {
       alert('You win!  That\'s the special number.  It was 17!');
+      correctUserGuessCount++;
       break;
     } else if (userNumberGuess > 17) {
       alert('That\'s too high.  Guesses remaining: ' + (4 - numOfGuesses));
@@ -74,14 +79,47 @@ function questionGuessNum() {
   }
 }
 
+
+// function interacts with user as they attempt to guess my favorite color
+function guessColor() {
+  // list of my favorite colors
+  var myFavColors = ['blue', 'cyan', 'seafoam green', 'purple', 'fuscia', 'green'];
+  // track num of guesses
+  var numOfGuesses = 0;
+  // keep track if user has guessed right answer
+  var flag;
+
+  while (numOfGuesses <= 6 && flag !== true) {
+    // get user guess
+    var userColorGuess = prompt('Try and guess my favorite color (guesses remaining: ' + (6 - numOfGuesses) + '):').toLowerCase();
+    for (var guess=0; guess < myFavColors.length; guess++) {
+      if (userColorGuess === myFavColors[guess]) {
+        alert(userColorGuess + ' is one of my favorite colors!  Well done.');
+        correctUserGuessCount++;
+        flag = true;
+      }
+    }
+    // user guessed wrong and guess count increases
+    numOfGuesses++;
+  }
+
+  // display all possible answers
+  var colorsMsg = myFavColors[0];
+  for (var color = 1; color < myFavColors.length; color++) {
+    colorsMsg = colorsMsg + ', ' + myFavColors[color];
+  }
+  alert('Here are all my favorite colors: ' + colorsMsg);
+}
+
+
 // function alerts user with final message based off the number of their correct answers
 function displayCustomMessage(count) {
   if (count > 4) {
-    alert('Hey, ' + user + '.  You answered most questions correctly.  Maybe you know me already!');
+    alert('Hey, ' + user + '.  You answered ' + correctUserGuessCount + '/7 questions correctly.  Well done!');
   } else if (count === 3) {
-    alert('Hey, ' + user + '.  You answered some questions correctly.  Maybe I haven\'t talked enough to you!');
+    alert('Hey, ' + user + '.  You answered ' + correctUserGuessCount + '/7 questions correctly.  Not bad...');
   } else if (count < 3) {
-    alert('Hey, ' + user + '.  You answered most questions wrong.  You don\'t know me well, but that\'s cool!');
+    alert('Hey, ' + user + '.  You answered ' + correctUserGuessCount + '/7 questions correctly  Refresh and try again!');
   }
 }
 

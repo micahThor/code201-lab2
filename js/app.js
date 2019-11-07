0880108a6971ef027629c532a061893d7b81245f
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-11-07 13:35:19 -0800

    refactored guessing game functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// userCount increases with each 'Y' or 'YES' answer
-var userCount = 0;
+// correctUserGuessCount increases with each 'Y' or 'YES' answer
+var correctUserGuessCount = 0;
 
 // get input from user
 var user = prompt('Welcome to my page.  Please enter your name.');
@@ -27,8 +27,11 @@ var cardGameQuestion = prompt('Okay, ' + user + '.  Do you think I like card gam
 evalQuestion(cardGameQuestion);
 // console.log('Likes card games: ', cardGameQuestion);
 
+// User attemps to guess the special number
+questionGuessNum();
+
 // Display custom message 
-displayCustomMessage(userCount);
+displayCustomMessage(correctUserGuessCount);
 
 
 //
@@ -39,9 +42,35 @@ displayCustomMessage(userCount);
 function evalQuestion(userGuess) {
   if (userGuess === 'Y' || userGuess === 'YES') {
     alert('That\'s correct!');
-    userCount++;
+    correctUserGuessCount++;
   } else {
     alert('That\'s incorrect');
+  }
+}
+
+// function interacts with user based of their guess of the special number of 17
+function questionGuessNum() {
+  // track num of guesses
+  var numOfGuesses = 1;
+
+  while (numOfGuesses <= 4) {
+    // get user's guess
+    var userNumberGuess = parseInt(prompt('Guess a number between 1 and 25.'));
+    // eval guess
+    if (userNumberGuess === 17) {
+      alert('You win!  That\'s the special number.  It was 17!');
+      break;
+    } else if (userNumberGuess > 17) {
+      alert('That\'s too high.  Guesses remaining: ' + (4 - numOfGuesses));
+      numOfGuesses++;
+    } else if (userNumberGuess < 17) {
+      alert('That\'s too low. Guesses remaining: ' + (4 - numOfGuesses));
+      numOfGuesses++;
+    }
+  }
+  // tell user they didn't guess correctly and give them answer
+  if (numOfGuesses > 4) {
+    alert('Thanks for playing but you didn\'t get the number.  It was 17!');
   }
 }
 
@@ -55,3 +84,4 @@ function displayCustomMessage(count) {
     alert('Hey, ' + user + '.  You answered most questions wrong.  You don\'t know me well, but that\'s cool!');
   }
 }
+
